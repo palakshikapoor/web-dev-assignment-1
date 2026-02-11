3c912221981f24f36828f808efb2093d47f4dc62
Palakshi <palakshikapoor03@gmail.com>
2026-02-11 13:40:15 +0530

    Added function deleteEvent and clearEvent

--- script.js
+++ script.js
@@ -43,3 +43,30 @@ function addEventCard(data) {
     list.appendChild(eventDiv);
 }
 
+function deleteEvent(button) {
+    button.parentElement.remove();
+
+    let list = document.getElementById("eventsList");
+    if (list.children.length === 0) {
+        list.innerHTML = "No events yet. Add your first event!";
+    }
+}
+
+function clearEvents() {
+    document.getElementById("eventsList").innerHTML =
+        "No events yet. Add your first event!";
+}
+
+function addSampleEvents() {
+    clearEvents();
+
+    formObj = {
+        title: "idea 3.0",
+        date: "2026-01-28",
+        category: "Social",
+        description: "it is best for innovation idea"
+    };
+
+    addEventCard(formObj);
+}
+
