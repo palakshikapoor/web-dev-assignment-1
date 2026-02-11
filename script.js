f2a576a553b119a7766d7b10af976c1967689ed3
Palakshi <palakshikapoor03@gmail.com>
2026-02-11 13:36:04 +0530

    Added function eventSubmitHandler

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ let formObj = {
     category: "",
     description: ""
 };
+
+function eventSubmitHandler(event) {
+    event.preventDefault();
+
+    formObj.title = event.target.title.value;
+    formObj.date = event.target.date.value;
+    formObj.category = event.target.category.value;
+    formObj.description = event.target.description.value;
+
+    if (formObj.title === "" || formObj.date === "") {
+        alert("Please enter event title and date");
+        return;
+    }
+
+    addEventCard(formObj);
+    event.target.reset();
+}
+
